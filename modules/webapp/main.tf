cc0be804e791c6c3e8afc0a92f47c37f1bc17d20
vamsi560 <vamsi.sapireddy@outlook.com>
2025-12-10 06:43:20 +0000

    Add Azure Web App Terraform config and CI/CD pipeline 2

---
+++ modules/webapp/main.tf
@@ -0,0 +1,51 @@
+// Azure Web App module boilerplate
+// Compliant with organizational standards
+
+variable "environment" { type = string }
+variable "region" { type = string }
+variable "resource_group_name" { type = string }
+variable "webapp_name" { type = string }
+variable "key_vault_name" { type = string }
+variable "tags" { type = map(string) }
+variable "app_service_plan_sku" { type = string }
+variable "runtime_stack" { type = string }
+variable "private_endpoint_enabled" { type = bool }
+variable "identity_type" { type = string }
+
+resource "azurerm_resource_group" "webapp_rg" {
+  name     = var.resource_group_name
+  location = var.region
+  tags     = var.tags
+}
+
+resource "azurerm_app_service_plan" "webapp_plan" {
+  name                = "${var.webapp_name}-plan"
+  location            = azurerm_resource_group.webapp_rg.location
+  resource_group_name = azurerm_resource_group.webapp_rg.name
+  sku {
+    tier = var.app_service_plan_sku == "B1" ? "Basic" : var.app_service_plan_sku == "S1" ? "Standard" : "PremiumV2"
+    size = var.app_service_plan_sku
+  }
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                = var.webapp_name
+  location            = azurerm_resource_group.webapp_rg.location
+  resource_group_name = azurerm_resource_group.webapp_rg.name
+  service_plan_id     = azurerm_app_service_plan.webapp_plan.id
+  tags                = var.tags
+
+  site_config {}
+
+  identity {
+    type = var.identity_type
+  }
+
+  app_settings = {
+    "ENVIRONMENT" = var.environment
+  }
+}
+
+output "webapp_url" {
+  value = azurerm_linux_web_app.webapp.default_hostname
+}
